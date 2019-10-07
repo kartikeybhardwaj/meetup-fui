c7c07ec9b7fdfb8546a09ef34009a343307f4318
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-07 21:53:17 +0530

    added unauthorized access page

---
+++ src/app/unauthorized-access/unauthorized-access.component.ts
@@ -0,0 +1,17 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+@Component({
+  selector: 'app-unauthorized-access',
+  templateUrl: './unauthorized-access.component.html',
+  styleUrls: ['./unauthorized-access.component.css']
+})
+export class UnauthorizedAccessComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
