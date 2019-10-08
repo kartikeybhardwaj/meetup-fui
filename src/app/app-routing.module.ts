39c1a99c80ef2db3a9f82c4a3a089b03d60830a1
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-08 21:58:11 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,8 +11,56 @@ import {
 import {
   PageNotFoundComponent
 } from './page-not-found/page-not-found.component';
+import {
+  LoginComponent
+} from './login/login.component';
+import {
+  SignupComponent
+} from './signup/signup.component';
+import {
+  LiveComponent
+} from './live/live.component';
+import {
+  PreviousComponent
+} from './previous/previous.component';
+import {
+  UpcomingComponent
+} from './upcoming/upcoming.component';
+import {
+  DescriptionComponent
+} from './description/description.component';
+import {
+  AllComponent
+} from './all/all.component';
+import {
+  AddComponent
+} from './add/add.component';
 
 const routes: Routes = [{
+  path: 'login',
+  component: LoginComponent
+}, {
+  path: 'signup',
+  component: SignupComponent
+}, {
+  path: 'add',
+  component: AddComponent
+}, {
+  path: 'all',
+  component: AllComponent
+}, {
+  path: 'live',
+  component: LiveComponent
+}, {
+  path: 'previous',
+  component: PreviousComponent
+}, {
+  path: 'upcoming',
+  component: UpcomingComponent
+}, {
+  path: 'description/:id',
+  component: DescriptionComponent
+}, {
   path: 'unauthorized-access',
   component: UnauthorizedAccessComponent
 }, {
@@ -21,7 +69,9 @@ const routes: Routes = [{
 }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
