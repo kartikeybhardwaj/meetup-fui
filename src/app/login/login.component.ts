88753145f08378ff4e7979d94b3ae7cd75462962
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-10 01:08:23 +0530

    login completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import {
 import {
   AppStorageService
 } from '../app.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -13,12 +14,70 @@ import {
 })
 export class LoginComponent implements OnInit {
 
+  username = '';
+  password = '';
+  message = '';
+  errorMessage = '';
+  isLoggingIn = false;
+
   constructor(
-    private appInfo: AppStorageService
+    private appInfo: AppStorageService,
+    private http: HttpClient
   ) {
     appInfo.headerText = 'Login';
   }
 
   ngOnInit() {}
+
+  validateLogin(): boolean {
+    let success = false;
+    if (this.username.length && this.password.length) {
+      success = true;
+      this.message = '';
+    } else {
+      this.message = 'Please enter username and password to login';
+    }
+    return success;
+  }
+
+  login(): void {
+    this.message = '';
+    this.errorMessage = '';
+    if (this.validateLogin()) {
+      this.isLoggingIn = true;
+      const reqUrl = 'http://localhost:3200/login';
+      const reqPayload = JSON.stringify(this.generateLoginRequestObject());
+      this.http.post(reqUrl, reqPayload, this.appInfo.headers).subscribe(
+        (response: any) => {
+          this.isLoggingIn = false;
+          if (response && response.responseId) {
+            if (response.responseId === 211) {
+              this.appInfo.user = response.returnData;
+              this.appInfo.headersWithAuth = {
+                headers: new HttpHeaders({
+                  'Content-Type': 'application/json',
+                  Authorization: 'Bearer ' + response.token
+                })
+              };
+              this.appInfo.localStorage.setItem('meetupToken', 'Bearer ' + response.token);
+            } else if (response.message) {
+              this.errorMessage = response.message;
+            }
+          }
+        },
+        (error: any) => {
+          this.isLoggingIn = false;
+          this.errorMessage = 'some error occurred';
+        });
+    }
+  }
+
+  generateLoginRequestObject(): any {
+    return {
+      username: this.username,
+      password: this.appInfo.encryptWithNumber(this.appInfo.encryptWithoutNumber(this.password)),
+      number: this.appInfo.encodingNumber
+    };
+  }
 
 }
