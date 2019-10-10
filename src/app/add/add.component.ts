6f82e43ab00c3f24a6b01a3e744607d407ccfe3c
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-10-10 16:47:16 +0530

    add meetup completed

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -17,6 +17,12 @@ import {
 import {
   MapsAPILoader
 } from '@agm/core';
+import {
+  HttpClient
+} from '@angular/common/http';
+import {
+  Router
+} from '@angular/router';
 
 declare var google: any;
 
@@ -34,14 +40,19 @@ export interface AddDialogData {
 })
 export class AddComponent implements OnInit {
 
-  startDate = '';
-  endDate = '';
+  message = '';
+  errorMessage = '';
+  isAdding = false;
 
+  title = '';
+  description = '';
+  startDate: any = '';
   startHour = '13';
   startMinute = '00';
-
+  endDate: any = '';
   endHour = '15';
   endMinute = '00';
+  isPrivate = false;
 
   hours: string[] = [
     '00', '01', '02', '03', '04',
@@ -57,8 +68,6 @@ export class AddComponent implements OnInit {
     '50', '55'
   ];
 
-  isPrivate = true;
-
   mapTitle = '';
   mapCountry = '';
   mapLat: number;
@@ -66,12 +75,91 @@ export class AddComponent implements OnInit {
 
   constructor(
     private appInfo: AppStorageService,
-    public mapDialog: MatDialog
+    public mapDialog: MatDialog,
+    private http: HttpClient,
+    private router: Router
   ) {
     appInfo.headerText = 'Add';
   }
 
   ngOnInit() {}
+
+  validateMeetup(): boolean {
+    let success = false;
+    this.title = this.title.trim();
+    this.description = this.description.trim();
+    this.mapTitle = this.mapTitle.trim();
+    if (!this.title.length || !this.description.length || this.startDate === '' || this.endDate === '') {
+      this.errorMessage = 'Please fill in all the required fields';
+    } else if (this.title.length < 4) {
+      this.errorMessage = 'Length of title should be more than 4';
+    } else if (this.description.length < 4) {
+      this.errorMessage = 'Length of description should be more than 4';
+    } else {
+      const startDateTime = this.startDate;
+      startDateTime.setHours(Number(this.startHour), Number(this.startMinute));
+      const endDateTime = this.endDate;
+      endDateTime.setHours(Number(this.endHour), Number(this.endMinute));
+      if (!(startDateTime.getTime() > new Date().getTime() && startDateTime.getTime() < endDateTime.getTime())) {
+        this.errorMessage = 'Start datetime should be greater than current datetime and End datetime should be greater than Start datetime';
+      } else {
+        success = true;
+      }
+    }
+    return success;
+  }
+
+  addMeetup(): void {
+    this.message = '';
+    this.errorMessage = '';
+    if (this.validateMeetup()) {
+      this.isAdding = true;
+      const reqUrl = 'http://localhost:3200/add-meetup';
+      const reqPayload = JSON.stringify(this.generateAddMeetupRequestObject());
+      this.http.post(reqUrl, reqPayload, this.appInfo.headersWithAuth).subscribe(
+        (response: any) => {
+          this.isAdding = false;
+          if (response && response.responseId) {
+            if (response.responseId === 211) {
+              this.router.navigate(['description/' + response.returnData._id.$oid]);
+            } else if (response.message) {
+              this.errorMessage = response.message;
+            }
+          }
+        },
+        (error: any) => {
+          this.isAdding = false;
+          this.errorMessage = 'some error occurred';
+        });
+    }
+  }
+
+  generateAddMeetupRequestObject(): any {
+    const startDateTime = this.startDate;
+    startDateTime.setHours(Number(this.startHour), Number(this.startMinute));
+    const endDateTime = this.endDate;
+    endDateTime.setHours(Number(this.endHour), Number(this.endMinute));
+    return {
+      title: this.title,
+      description: this.description,
+      location: {
+        title: this.mapTitle,
+        country: this.mapCountry,
+        latitude: this.mapLat,
+        longitude: this.mapLng
+      },
+      timeline: {
+        from: startDateTime.toISOString(),
+        to: endDateTime.toISOString()
+      },
+      isPrivate: this.isPrivate,
+      joinedBy: [this.appInfo.user._id.$oid],
+      metadata: {
+        createdBy: this.appInfo.user._id.$oid,
+        createdOn: new Date().toISOString()
+      }
+    };
+  }
 
   openMapDialog(): void {
     // tslint:disable-next-line: no-use-before-declare
